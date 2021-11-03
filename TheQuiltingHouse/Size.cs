d55fcfe7593419ff39476643f26a77a408f993e6
Collin-S <csudik@yahoo.com>
2021-11-03 19:09:58 -0400

    added PatternSize Style and quilt size)
    git status
    q
    l

---
+++ TheQuiltingHouse/Size.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheQuiltingHouse
+{
+    internal class Size
+    {
+    }
+}
